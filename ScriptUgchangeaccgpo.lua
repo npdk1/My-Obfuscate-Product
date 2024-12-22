69845dd1ef43785728ad518b01de6f3b64edb9bc
npdk1 <huysieu1302@gmail.com>
2024-12-22 22:41:54 +0700

    Update ScriptUgchangeaccgpo.lua

--- ScriptUgchangeaccgpo.lua
+++ ScriptUgchangeaccgpo.lua
@@ -1,3 +1,11 @@
+Code by Rokid Owners
 task.wait(30)
 getgenv().Level = 1
 loadstring(game:HttpGet("https://raw.githubusercontent.com/RokidManager/neyoshiiuem/refs/heads/main/GPO-ChangeAcc.lua"))()
+
+Code by npdk
+-- Cấu hình target level và thời gian kiểm tra từ bên ngoài
+getgenv().TargetLevel = 1 -- Đặt target level mặc định 
+getgenv().Delay = 5 -- Thời gian chờ giữa các lần kiểm tra (giây)
+-- Tải và thực thi script từ URL raw GitHub
+loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/My-Obfuscate-Product/refs/heads/main/changeacc-gpo.lua"))()
