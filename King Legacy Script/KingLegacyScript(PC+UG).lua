73ebec6d2c310d902a08a6d25be22900ef49107f
npdk1 <huysieu1302@gmail.com>
2024-12-23 18:19:13 +0700

    Create KingLegacyScript(PC+UG).lua

---
+++ King Legacy Script/KingLegacyScript(PC+UG).lua
@@ -0,0 +1,4 @@
+#For PC USer
+getgenv().TargetLevel = 100 -- Cấp độ mục tiêu
+getgenv().Delay = 1 -- Thời gian chờ giữa các lần kiểm tra (tính bằng giây)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/npdk1/My-Obfuscate-Product/refs/heads/main/King%20Legacy%20Script/obfKingLegacyYummyChange.txt"))()
